78598ea62b9cc90f64c557e5eead9c8a9a92db7a
任杰06559 <renjie@navinfo.com>
2019-10-17 14:44:26 +0800

    a example for simple linear regression, which input a vector of pair(x,y),then solve print the B0 and B1 which y=B0+B1*x

---
+++ machineLearning/simpleLinearRegression/simpleLinearRegression.cpp
@@ -0,0 +1,53 @@
+﻿#include <random>
+#include <ceres/ceres.h>
+#include "simpleLinearRegression.h"
+
+SimpleLinearRegression::SimpleLinearRegression(const std::vector<std::pair<double, double> >& data)
+    :_data(data)
+{
+
+}
+
+SimpleLinearRegression::~SimpleLinearRegression()
+{
+
+}
+
+void SimpleLinearRegression::solve()
+{
+    double B0, B1;
+    ceres::Problem problem;
+    for (auto m : _data) {
+        problem.AddResidualBlock(
+            new ceres::AutoDiffCostFunction<SimpleLinearRegressionCost, 1, 1, 1>(
+                new SimpleLinearRegressionCost(m.first, m.second)
+                ),
+            nullptr,
+            &B0, &B1
+        );
+    }
+
+    ceres::Solver::Options options;
+    options.linear_solver_type = ceres::DENSE_SCHUR;
+    options.minimizer_progress_to_stdout = true;
+
+    ceres::Solver::Summary summary;
+    ceres::Solve(options, &problem, &summary);
+
+    std::cout << summary.BriefReport() << std::endl;
+    std::cout << "estimated a, b = " << B0 << "," << B1 << std::endl;
+}
+
+
+
+int main(int argc, char* argv[]) {
+    std::vector<std::pair<double, double> > data;
+    data.emplace_back(1,1);
+    data.emplace_back(2,3);
+    data.emplace_back(4,3);
+    data.emplace_back(3,2);
+    data.emplace_back(5,5);
+    SimpleLinearRegression simpleLinearRegression(data);
+    simpleLinearRegression.solve();
+	return 0;
+}
---
+++ machineLearning/simpleLinearRegression/simpleLinearRegression.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+
+class SimpleLinearRegression
+{
+public:
+    SimpleLinearRegression(const std::vector<std::pair<double, double> >& data);
+    ~SimpleLinearRegression();
+    void solve();
+
+private:
+    std::vector<std::pair<double, double> > _data;
+};
+
+struct SimpleLinearRegressionCost
+{
+    const double _x, _y;
+
+    SimpleLinearRegressionCost(double x, double y) :_x(x), _y(y) {
+
+    }
+
+    template<typename T>
+    bool operator()(const T* const B0, const T* const B1, T* residual) const {
+        residual[0] = T(_y) - (B0[0] + B1[0] * T(_x));
+        return true;
+    }
+};
